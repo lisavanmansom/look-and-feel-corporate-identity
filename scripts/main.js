96e982722e494ad30996290297ed757cc8c06487
lisavanmansom <lisavanmansom@gmail.com>
2023-11-20 11:44:35 +0100

    van alles
    jajajajajja

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+
+const themeToggleBtn = document.querySelector('.theme-toggle');
+
+const theme = localStorage.getItem('theme');
+
+if (theme) { 
+    document.body.classList.add('dark-mode');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'darkmode'); localStorage.removeItem('theme2');
+    } else {
+        localStorage.removeItem('theme');
+    }
+});
+
+
+
+const themecontrastToggleBtn = document.querySelector('.theme-toggle-contrast');
+
+const theme2 = localStorage.getItem('theme2');
+
+if (theme2) { 
+    document.body.classList.add('contrast-mode');
+}
+
+themecontrastToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('contrast-mode');
+    if (document.body.classList.contains('contrast-mode')) {
+        localStorage.setItem('theme2', 'contrast-mode'); localStorage.removeItem('theme');
+    } else {
+        localStorage.removeItem('theme2');
+    }
+
+});
