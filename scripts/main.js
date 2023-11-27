36bac23007c7b03a5aa8e1c21c999027fa1c8f0e
Hadil24A <hadil.ahmed@hva.nl>
2023-11-27 12:33:47 +0100

    dark mode, popup in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -48,3 +48,15 @@ themecontrastToggleBtn.addEventListener('click', () => {
     }
 
 });
+
+
+ 
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
